f7dfd92f4dd43b75f01c11e2ccbd2c43f618c8b7
Matthew Petro <matthew.petro@aa.com>
2018-10-04 23:34:54 -0700

    Code cleanup and fix for incorrect dial readings on correct answers

--- resistance.ino
+++ resistance.ino
@@ -32,7 +32,7 @@ static const int BRIGHTNESS = 0;
 static const int ROTATION = ROTATION_HEADER_TOP;
 static const int SCROLL_SPEED = SCROLL_FAST;
 
-static const char ANSWERS[10][10] = {
+static const char RESPONSES[10][10] = {
     { '\0', '\0', '\0', '\0', 'G' , '\0', 'B' , '\0', '\0', '\0', },
 	{ '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', 'K' , '\0', },
 	{ 'J' , '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', },
@@ -46,53 +46,64 @@ static const char ANSWERS[10][10] = {
 };
 
 Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+int previousButtonInput = HIGH;
 
-void setup()
-{
+void setup() {
 	Serial.begin(9600);
 	Serial.println("Resistance clue starting");
 
-	matrix.begin(I2C_ADDRESS); // pass in the address
+	matrix.begin(I2C_ADDRESS);
 	matrix.setRotation(ROTATION);
 	matrix.setBrightness(BRIGHTNESS);
-	matrix.clear();
-	matrix.writeDisplay();
+	clearDisplay();
 
 	pinMode(BUTTON_INPUT, INPUT_PULLUP);
 }
 
-void loop()
-{
-	int buttonInput = digitalRead(BUTTON_INPUT);
-	if (buttonInput == HIGH) {
-		matrix.clear();
-		matrix.writeDisplay();
-	}
-	else
-	{
-		unsigned int dialInputs[2];
-		dialInputs[0] = analogRead(DIAL0_INPUT);
-		dialInputs[1] = analogRead(DIAL1_INPUT);
-
-		unsigned int dialValues[2];
-		dialValues[0] = translateDialInput(dialInputs[0]);
-		dialValues[1] = translateDialInput(dialInputs[1]);
-
-		char answerToDisplay = mapDialsToAnswer(dialValues);
-
-		if (answerToDisplay == '\0')
-		{
-			scrollText("TRY AGAIN");
-		}
-		else
-		{
-			displayCharacter(answerToDisplay);
-		}
+void loop() {
+	int currentButtonInput = digitalRead(BUTTON_INPUT);
+	if (currentButtonInput != previousButtonInput) {
+		if (currentButtonInput == HIGH) {
+	    	clearDisplay();
+	    }
+	    else {
+    		unsigned int dialValues[2];
+			readDialValues(dialValues);
+			char responseToDisplay = mapDialsToResponse(dialValues);
+			displayResponse(responseToDisplay);
+	    }
+		previousButtonInput = currentButtonInput;
 	}
 }
 
-unsigned int translateDialInput(unsigned int analogReading)
-{
+void clearDisplay() {
+	matrix.clear();
+	matrix.writeDisplay();
+}
+
+void readDialValues(unsigned int dialValues[]) {
+	unsigned int dialInputs[2];
+	dialInputs[0] = analogRead(DIAL0_INPUT);
+	dialInputs[1] = analogRead(DIAL1_INPUT);
+
+	Serial.print("Dial 0 - ");
+	dialValues[0] = translateDialInput(dialInputs[0]);
+    Serial.print("  ");
+	Serial.print("Dial 1 - ");
+	dialValues[1] = translateDialInput(dialInputs[1]);
+    Serial.println("");
+}
+
+void displayResponse(char responseToDisplay) {
+    if (responseToDisplay == '\0') {
+        scrollText("TRY AGAIN");
+    }
+	else {
+        displayCharacter(responseToDisplay);
+    }
+}
+
+unsigned int translateDialInput(unsigned int analogReading) {
 	// Translate ADC value from 0-1023 to 0-9.
 	// This implements the proportion
 	// input/1023 = val/9.
@@ -100,18 +111,26 @@ unsigned int translateDialInput(unsigned int analogReading)
 	// We're adding 56 (1/2 of 113) to the input value, so that the
 	// input is in the middle of the window, rather than right at the edge, so values
 	// are stable and solid.
-	return (analogReading + 56) * 9 / 1023;
+	float floatVal = (analogReading + 56) * float(9) / float(1023);
+	unsigned int intVal = (analogReading + 56) * 9 / 1023;
+
+    Serial.print("intVal: ");
+	Serial.print(intVal);
+	Serial.print(", floatVal: ");
+	Serial.print(floatVal);
+	Serial.print(", analog: ");
+	Serial.print(analogReading);
+
+	return intVal;
 }
 
-void displayCharacter(char character)
-{
+void displayCharacter(char character) {
 	matrix.clear();
 	matrix.drawChar(X_POSITION, Y_POSITION, character, LED_GREEN, LED_OFF, SIZE_MULTIPLIER);
 	matrix.writeDisplay();
 }
 
-void scrollText(String text)
-{
+void scrollText(String text) {
 	const int CHARACTER_PIXEL_WIDTH = 6;
 	matrix.setTextColor(LED_RED, LED_OFF);
 	matrix.setTextSize(SIZE_MULTIPLIER);
@@ -119,8 +138,7 @@ void scrollText(String text)
 	int textWidthInPixels = text.length() * CHARACTER_PIXEL_WIDTH;
 	displayText(X_POSITION, Y_POSITION, text);
 	delay(SCROLL_SPEED * 3);
-	for (int x = X_POSITION; x >= X_POSITION - textWidthInPixels; x--)
-	{
+	for (int x = X_POSITION; x >= X_POSITION - textWidthInPixels; x--) {
 		displayText(x, Y_POSITION, text);
 		delay(SCROLL_SPEED);
 	}
@@ -133,6 +151,6 @@ void displayText(int xPos, int yPos, String text) {
 	matrix.writeDisplay();
 }
 
-char mapDialsToAnswer(unsigned int dialValues[]) {
-	return ANSWERS[dialValues[0]][dialValues[1]];
+char mapDialsToResponse(unsigned int dialValues[]) {
+	return RESPONSES[dialValues[0]][dialValues[1]];
 }
